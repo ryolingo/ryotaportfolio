408a437a9952b75d6ef5f99e592d05fe0add8168
ryolingo <ryomatsuya04@gmail.com>
2024-09-26 20:28:20 +0900

    WorkCardをpropsを渡して表示

---
+++ src/app/components/organisms/Works.tsx
@@ -0,0 +1,18 @@
+import { Box, Grid } from "@mui/material";
+
+import { WorkList } from "../../data/WorkList";
+import WorkCard from "../molecule/WorksCard";
+
+const Works = () => {
+  return (
+    <Box>
+      {WorkList.map((workCard) => (
+        <Grid item key={workCard.imagePath}>
+          <WorkCard workCard={workCard} />
+        </Grid>
+      ))}
+    </Box>
+  );
+};
+
+export default Works;
