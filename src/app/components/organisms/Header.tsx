82b32528f2a58b68a79c50ece1d4d29faf0c7c72
ryolingo <ryomatsuya04@gmail.com>
2024-09-27 00:11:16 +0900

    headerにicon追加

--- src/app/components/organisms/Header.tsx
+++ src/app/components/organisms/Header.tsx
@@ -1,18 +1,46 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+"use client";
+import { AppBar, Container, styled, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
     <AppBar
-      position="relative"
+      position="fixed"
       sx={{
         backgroundColor: "white",
         color: "black",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        borderBottom: "1px solid #e0e0e0",
+        height: "65px",
       }}
     >
       <Container>
         <Toolbar>
-          <Typography>亮太のポートフォリオ</Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              fontSize: "1.5rem",
+              paddingLeft: "1rem",
+              letterSpacing: "0.1em",
+              mt: "0.5rem",
+            }}
+          >
+            Portfolio
+          </Typography>
+          <StyledIcon>
+            <Link href="https://github.com/ryolingo" target="blank">
+              <StyledImage
+                src={"/images/header/github.png"}
+                alt="githubアイコン"
+              />
+            </Link>
+            <Link href={"https://x.com/ryolongo"} target="blank">
+              <StyledImage src={"/images/header/x.png"} alt="twitterアイコン" />
+            </Link>
+          </StyledIcon>
         </Toolbar>
       </Container>
     </AppBar>
@@ -20,3 +48,24 @@ export const Header = () => {
 };
 
 export default Header;
+
+const StyledIcon = styled("div")(({ theme }) => ({
+  marginLeft: "auto", // アイコンを右に寄せる
+  marginRight: "3%",
+  gap: "1rem", // アイコンの間隔
+  display: "flex",
+  flexDirection: "row",
+  [theme.breakpoints.down("sm")]: {
+    marginRight: "1.7%",
+    gap: "1rem",
+  },
+}));
+
+const StyledImage = styled("img")(({ theme }) => ({
+  width: "3rem",
+  height: "3rem",
+  [theme.breakpoints.down("sm")]: {
+    width: "1.5rem",
+    height: "1.5rem",
+  },
+}));
