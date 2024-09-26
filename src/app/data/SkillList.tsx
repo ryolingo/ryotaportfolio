f0532ecd8162f9534d657057bdb1ba62f32e4059
ryolingo <ryomatsuya04@gmail.com>
2024-09-26 20:59:35 +0900

    SkillListtの作成

---
+++ src/app/data/SkillList.tsx
@@ -0,0 +1,36 @@
+export type Skill = {
+  imagePath: string;
+  title: string;
+};
+
+export const SkillList: Skill[] = [
+  {
+    imagePath: "html.png",
+    title: "HTML",
+  },
+  {
+    imagePath: "css.png",
+    title: "CSS",
+  },
+  {
+    imagePath: "javascript.png",
+    title: "JavaScript",
+  },
+  {
+    imagePath: "react.png",
+    title: "React",
+  },
+  {
+    imagePath: "next.js.png",
+    title: "Next.js",
+  },
+  {
+    imagePath: "typescript.png",
+    title: "TypeScript",
+  },
+
+  {
+    imagePath: "supabase.png",
+    title: "supabase",
+  },
+];
