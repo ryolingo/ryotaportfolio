ef5bdd4cfca9a6da581dd7fd2fa5fb464df32551
ryolingo <ryomatsuya04@gmail.com>
2024-09-26 20:58:52 +0900

    不要な文削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Header from "./components/organisms/Header";
 import Footer from "./components/organisms/Footer";
 import Introduction from "./components/organisms/Introduction";
 import TabPanel from "./components/molecule/TabPanel";
-import { Timeline } from "@mui/icons-material";
 
 export default function Home() {
   return (
