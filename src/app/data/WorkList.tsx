ba56ffc27ede00f7879eec277ada14d33b479a3e
ryolingo <ryomatsuya04@gmail.com>
2024-09-26 20:28:29 +0900

    WorkListの作成

---
+++ src/app/data/WorkList.tsx
@@ -0,0 +1,35 @@
+export type Work = {
+  imagePath: string;
+  title: string;
+  details: {
+    content: string;
+    description: string;
+    language: string;
+    github: string;
+  };
+};
+
+export const WorkList: Work[] = [
+  {
+    imagePath: "typinggame.png",
+    title: "TOEIC Vocabulary Typing Game",
+    details: {
+      content: "TOEICの英単語をTypingGameで学習できるサイト",
+      description:
+        "初めての個人開発。TOEICの英単語を楽しく学習できるように作成しました。",
+      language: "HTML,CSS / JavaScript / React",
+      github: "https://github.com/ryolingo/Typing-game",
+    },
+  },
+  {
+    imagePath: "keyaki2024.png",
+    title: "欅祭アンケート",
+    details: {
+      content: "学校の文化祭のコメントを投稿できるwebサイト",
+      description:
+        "私が在籍している成蹊大学の文化祭で、出展するPeachTechのコメントをいただくために作成しました。初めてのチーム開発で、Next.jsとSupabaseを使用しました。",
+      language: "Typescript / Next.js / Supabase",
+      github: "https://github.com/ryolingo/Keyaki2024",
+    },
+  },
+];
