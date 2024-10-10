732874bf695eb8588480d9952358981272a72924
ryolingo <ryomatsuya04@gmail.com>
2024-10-10 21:43:03 +0900

    layout.tsxの解決t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="jn">
-      <body className={inter.className}>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
