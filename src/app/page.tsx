2785a1f0bb22c0e10ab6465ead0e81053c8ef0df
ryolingo <ryomatsuya04@gmail.com>
2024-11-08 18:17:33 +0900

    Introduction追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
+"use client";
 import Header from "./components/organisms/Header";
 import Footer from "./components/organisms/Footer";
 import Introduction from "./components/organisms/Introduction";
 import TabPanel from "./components/molecule/TabPanel";
+import { useEffect, useState } from "react";
+import IntroAnimation from "./components/molecule/Animation";
 
 export default function Home() {
+  const [showIntro, setShowIntro] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setShowIntro(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Header />
-      <Introduction />
-      <TabPanel />
-      <Footer />
+      {showIntro ? (
+        <IntroAnimation />
+      ) : (
+        <>
+          <Header />
+          <Introduction />
+          <TabPanel />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
