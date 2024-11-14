4fdff7ae7ede497c78577b963529f539cf45dfaa
ryolingo <ryomatsuya04@gmail.com>
2024-11-14 16:49:49 +0900

    TOYPE追加

--- src/app/data/WorkList.tsx
+++ src/app/data/WorkList.tsx
@@ -11,16 +11,28 @@ export type Work = {
 
 export const WorkList: Work[] = [
   {
-    imagePath: "typinggame.png",
-    title: "TOEIC Vocabulary Typing Game",
+    imagePath: "toype.png",
+    title: "TOYPE",
     details: {
       content: "TOEICの英単語をTypingGameで学習できるサイト",
       description:
-        "初めての個人開発。TOEICの英単語を楽しく学習できるように作成しました。",
+        "toeic-tyinggameをtyoescriptとNext.jsとfirebaseを使用して作成しました。ログイン機能を追加し、",
       language: "HTML,CSS / JavaScript / React",
-      github: "https://github.com/ryolingo/Typing-game",
+      github: "https://toeic-typing-game.vercel.app/",
     },
   },
+  {
+    imagePath: "fashonista.png",
+    title: "FASHONISTA",
+    details: {
+      content: "天気と季節からコーディネートを提案してくれるwebアプリ",
+      description:
+        " Next.jsとTypescriptとshadcnを使用して作成しました。「今日何着ようという朝の悩みを解決するためのアプリ」",
+      language: "Typescript / Next.js / Firebase / shadcn/",
+      github: "https://fashonista-pi.vercel.app/",
+    },
+  },
+
   {
     imagePath: "keyaki2024.png",
     title: "欅祭アンケート",
@@ -43,15 +55,16 @@ export const WorkList: Work[] = [
       github: "https://ryotaportfolio.vercel.app/",
     },
   },
+
   {
-    imagePath: "fashonista.png",
-    title: "FASHONISTA",
+    imagePath: "typinggame.png",
+    title: "TOEIC Vocabulary Typing Game",
     details: {
-      content: "天気と季節からコーディネートを提案してくれるwebアプリ",
+      content: "TOEICの英単語をTypingGameで学習できるサイト",
       description:
-        " Next.jsとTypescriptとshadcnを使用して作成しました。「今日何着ようという朝の悩みを解決するためのアプリ」",
-      language: "Typescript / Next.js / Firebase / shadcn/",
-      github: "https://fashonista-pi.vercel.app/",
+        "初めての個人開発。TOEICの英単語を楽しく学習できるように作成しました。",
+      language: "HTML,CSS / JavaScript / React",
+      github: "https://github.com/ryolingo/Typing-game",
     },
   },
 ];
